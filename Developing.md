899da56eefd5e81f0edd2d8d1095b1db73d5c63e
lenz <lenz@mac>
2019-05-19 12:55:23 +0200

    Dev env

--- Developing.md
+++ Developing.md
@@ -1,13 +1,28 @@
 # Dev notes
 
-lein fig -- --build dev --repl
+	lein fig -- --build dev --repl
+
+or
+
+	lein figdev
+
+
+Check and fix sources formatting
+
+	lein fix
+
+and 
+
+	lein fix project.clj
+
+
+
 
 Nella repl:
 
 
-(use 'scalything.core)
-
-(reset! S "bar")
+	(use 'scalything.core)
+	(reset! SX "bar")
 
 
 
--- project.clj
+++ project.clj
@@ -1,20 +1,21 @@
 (defproject scalything "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [reagent "0.8.1"]]
-:aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]}
-:source-paths ["src"]
+  :aliases {"fig" ["trampoline" "run" "-m" "figwheel.main"]
+            "figdev" ["trampoline" "run" "-m" "figwheel.main" "--" "--build" "dev" "--repl"]
+            "fix" ["cljfmt" "fix"]}
+  :plugins [[lein-cljfmt "0.6.4"]]
+  :source-paths ["src"]
 
+  :profiles
+  {:dev
+   {:resource-paths ["target"]
+    :clean-targets ^{:protect false} ["target"]
 
-  :profiles 
-    {:dev 
-      {:resource-paths ["target"]
-                   :clean-targets ^{:protect false} ["target"]
-
-
-      	:dependencies [[org.clojure/clojurescript "1.10.339"]
-                      [com.bhauman/figwheel-main "0.2.0"]
+    :dependencies [[org.clojure/clojurescript "1.10.339"]
+                   [com.bhauman/figwheel-main "0.2.0"]
                       ;; optional but recommended
-                      [com.bhauman/rebel-readline-cljs "0.1.4"]]}})
+                   [com.bhauman/rebel-readline-cljs "0.1.4"]]}})
 
 
 
